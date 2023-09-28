53ddc1da1f7677b1ad742fb3035efd8831f8cb41
NilsGke <n.j.goeke@gmail.com>
2023-09-28 17:41:30 +0200

    car

---
+++ OOP/Car/Auto.java
@@ -0,0 +1,96 @@
+public class Auto {
+
+  private String Kennzeichen;
+  private double Kilometerstand;
+  private double Tankvolumen;
+  private double Verbrauch;
+  private double Tankinhalt;
+
+  public Auto(String Kennzeichen, double Tankvolumen, double Verbrauch) {
+    this.Kennzeichen = Kennzeichen;
+    this.Tankvolumen = Tankvolumen;
+    this.Verbrauch = Verbrauch;
+    Tankinhalt = 0;
+    Kilometerstand = 0;
+  }
+
+  public String getKennzeichen() {
+    return Kennzeichen;
+  }
+
+  public double getKilometerstand() {
+    return Kilometerstand;
+  }
+
+  public double getTankinhalt() {
+    return Tankinhalt;
+  }
+
+  public void tanken(double Menge) {
+    if (Menge > this.Tankvolumen - this.Tankinhalt) {
+      System.out.println(
+        "Du hast " +
+        (Menge - this.Tankvolumen - this.Tankinhalt) +
+        " Liter zu viel getankt 🤨"
+      );
+      this.Tankinhalt = this.Tankvolumen;
+    } else {
+      this.Tankinhalt += Menge;
+    }
+  }
+
+  public void vollTanken() {
+    this.Tankinhalt = this.Tankvolumen;
+  }
+
+  public double calcStreckeBisLeer() {
+    return this.Tankinhalt / (this.Verbrauch / 100);
+  }
+
+  public void fahren(double Strecke) {
+    double VerbrauchFuerStrecke = Strecke * (this.Verbrauch / 100);
+
+    System.out.print("Brrrrrrrrrrrrr 🚗");
+
+    if (VerbrauchFuerStrecke > this.Tankinhalt) {
+      Strecke = this.Tankinhalt / (this.Verbrauch / 100);
+      this.Kilometerstand += Strecke;
+      this.Tankinhalt = 0;
+      System.out.println("Die Strecke war zu lang, dein Tank ist nun leer 😯");
+    } else {
+      this.Kilometerstand += Strecke;
+      this.Tankinhalt -= VerbrauchFuerStrecke;
+    }
+  }
+
+  public void leerFahren() {
+    this.Kilometerstand += this.Tankinhalt / (this.Verbrauch / 100);
+    this.Tankvolumen = 0;
+  }
+
+  public double aktuellerVerbrauch(double Strecke, double Menge) {
+    // Aufgabe: Implementiere eine Funktion aktuellerVerbrauch(double Strecke, double Menge) die aus Strecke und Menge den Verbrauch berechnet.
+    // was wollen die hier?
+    return Strecke * Menge; // ?
+  }
+
+  public void langeStreckeFahren(double Strecke) {
+    int Tankpausen = 0;
+
+    while (Strecke > 0) {
+      double streckeBisLeer = calcStreckeBisLeer();
+      if (Strecke > streckeBisLeer) {
+        fahren(streckeBisLeer);
+        Strecke -= streckeBisLeer;
+        this.vollTanken();
+        Tankpausen++;
+      } else {
+        this.Kilometerstand -= Strecke;
+        this.Tankinhalt -= Strecke / (this.Verbrauch / 100);
+        Strecke = 0;
+      }
+    }
+
+    System.out.println("du musstest " + Tankpausen + " mal tanken ⛽");
+  }
+}
---
+++ OOP/Car/Console.java
@@ -0,0 +1,8 @@
+public class Console {
+
+  public static void main(String[] args) {
+    Auto bmw = new Auto("WI GG 69", 60.0, 6.0);
+    bmw.vollTanken();
+    bmw.fahren(6_000_000);
+  }
+}
